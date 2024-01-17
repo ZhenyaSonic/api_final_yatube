f704c5ef2ca4f45dcf214a62147a2bba0958feed
Евгений Братанов <sonik.zhenyaa@gmail.com>
2024-01-17 10:36:20 +0300

    Вторая отправка

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, permissions, pagination
+from django.db.models import Q
 
-from posts.models import Group, Post, Comment
+from posts.models import Group, Post, Follow
 from .permissions import IsAuthorOrReadOnly
 from .serializers import (
     CommentSerializer,
@@ -15,11 +16,13 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.select_related('author')
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (
+        IsAuthorOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    )
     pagination_class = pagination.LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -28,19 +31,20 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (
+        IsAuthorOrReadOnly,
+        permissions.IsAuthenticatedOrReadOnly
+    )
 
     def get_post(self):
         return get_object_or_404(Post, pk=self.kwargs.get('post_id'))
 
     def get_queryset(self):
-        return Comment.objects.filter(
-            post=self
-            .get_post()).select_related('author')
+        post = self.get_post()
+        return post.comments.all().select_related('author')
 
     def perform_create(self, serializer):
-        post = self.get_post()
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
 
 
 class FollowViewSet(viewsets.ModelViewSet):
